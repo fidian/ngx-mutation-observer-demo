98409f404245ed7d206dc7196114491665ece75b
Tyler Akins <fidian@rumkin.com>
2019-09-10 15:31:28 -0500

    Fixing to use correct path for the demo site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { NgxMutationObserverModule } from '../../../ngx-mutation-observer/src/public-api';
+import { NgxMutationObserverModule } from 'ngx-mutation-observer';
 
 @NgModule({
     declarations: [
